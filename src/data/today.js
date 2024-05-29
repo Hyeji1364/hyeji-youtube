5182e4424c406a4e5b8ce47fc36e095a426df204
Hyeji1364 <161557112+Hyeji1364@users.noreply.github.com>
2024-05-29 13:04:56 +0900

    추천 영상 작업

---
+++ src/data/today.js
@@ -0,0 +1,77 @@
+import todayView01 from "../assets/img/today/todayView01.webp";
+import todayView02 from "../assets/img/today/todayView02.webp";
+import todayView03 from "../assets/img/today/todayView03.webp";
+import todayView04 from "../assets/img/today/todayView04.webp";
+import todayView05 from "../assets/img/today/todayView05.webp";
+import todayView06 from "../assets/img/today/todayView06.webp";
+// import todayView07 from "../assets/img/today/todayView07.webp";
+// import todayView08 from "../assets/img/today/todayView08.webp";
+
+export const todayText = [
+    {
+        img: todayView01,
+        title: "🍡 오사카에 가서 먹기만 하는 사람들이 있다 ? YES! 그게 바로 나! 이것이 바로 먹는 여행! 먹깨비를 보여주마!",
+        desc: "#유니버설스튜디오재팬 #오사카여행 #마이리얼트립",
+        author: "해쭈 [HAEJOO]",
+        channelId: "UC6KwCU8Y8Uw4h_Q0ptLZkqw",
+        date: " 2023. 5. 19.",
+        page: "/japan"
+    }, {
+        img: todayView02,
+        title: "여행 딴따라들과 함께한 가오슝 배낭여행 시작 【대만1】",
+        desc: "첫 중화권 나라!!!",
+        author: "빠니보틀 Pani Bottle",
+        channelId: "UCNhofiqfw5nl-NeDJkXtPvw",
+        date: "2023. 11. 2.",
+        page: "/taiwan"
+    }, {
+        img: todayView03,
+        title: "(1시간) 백종원이 추천하는 베트남 현지 음식🤤 이 영상 하나로 베트남 준비 끝! | #스트리트푸드파이터2 #디제이픽",
+        desc: "취향저격! 당신이 원하던 그 플레이리스트 [#디제이픽😎] #스트리트푸드파이터2 #Diggle #디글 ",
+        author: "디글: Diggle",
+        channelId: "UCWYzc_p0GgfCepIWDHGFmEg",
+        date: "2022. 10. 1.",
+        page: "/vietnam"
+    }, {
+        img: todayView04,
+        title: "eng) 알콩달콩 4주년 기념 홍콩 여행 vlog ep.1🇭🇰 : 홍콩, 인생 여행지 되다?",
+        desc: "레트로 갬성 가득 카페 투어🧋ㅣ초이홍 아파트ㅣ2층버스ㅣ침사추이ㅣ란퐁유엔",
+        author: "토모토모TomoTomo",
+        channelId: "UCoQy2wS5aiKCk-rUXiLS-vQ",
+        date: "2023. 3. 28.",
+        page: "/honkong"
+    }, {
+        img: todayView05,
+        title: "VLOGㅣ싱가폴쇼핑,카페,향수,인생샷,수영장(feat.문별)",
+        desc: "월드투어 중 싱가포르에서 시간이 여유 있게 남아서 잠시 휴식(?)을 즐겼어요:)",
+        author: "솔라시도 solarsido",
+        channelId: "UCNx0TXIuGVEA_TWB5H2I6Lg",
+        date: "2023. 3. 2.",
+        page: "/singapore"
+    }, {
+        img: todayView06,
+        title: "[뉴욕여행 브이로그] EP1. 뉴욕만 n번 간 여자의 나홀로 9박 10일 여행 🗽🩵 | 입국심사부터 엠파이어 스테이트 빌딩 전망대까지, 첫 날 일정 모조리 담아봤어요 ✨",
+        desc: "안녕하세요 :) 지난 11월에 9박 10일로 뉴욕여행을 다녀왔습니다.",
+        author: "Day Kim 데이 킴",
+        channelId: "UCRoUvGzhYuBaudImV8MBwDQ",
+        date: "2023. 12. 31.",
+        page: "/usa"
+    }
+    // , {
+    //     img: todayView07,
+    //     title: "웹디자인 기능사는 이 영상으로 준비 끝",
+    //     desc: "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+    //     author: "webstoryboy",
+    //     channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    //     date: "2023.07.07",
+    //     page: "/australia"
+    // }, {
+    //     img: todayView08,
+    //     title: "디자인 부터 코딩까지 준비 완료",
+    //     desc: "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
+    //     author: "webstoryboy",
+    //     channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    //     date: "2023.07.07",
+    //     page: "/australia"
+    // }
+]
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { todayText } from '../data/today'
+import { Link } from 'react-router-dom'
+
 const Today = () => {
     return (
         <Main
             title="추천영상"
             desciprion="추천 영상입니다.">
-            Today
+
+            <section id='todayPage'>
+                <h2>🎈 오늘의 추천 영상입니다.</h2>
+
+                {todayText.map((today, key) => (
+                    <div className='today__inner' key={key}>
+                        <div className='today__thumb play__icon'>
+                            <Link to={today.page}>
+                                <img src={today.img} alt={today.title} />
+                            </Link>
+                        </div>
+                        <div className='today__text'>
+                            <span className='today'>today!</span>
+                            <h3 className='title'>
+                                <Link to={today.page}>
+                                    {/* {today.title} */}
+                                </Link>
+                                {today.title}</h3>
+                            <p className='desc'>{today.desc}</p>
+                            <div className='info'>
+                                <span className='author'>
+                                    <Link to={`/channel/${today.channelId}`}>
+                                        {today.author}
+                                    </Link>
+                                </span>
+                                <span className='date'>{today.date}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
         </Main>
     )
 }
